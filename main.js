d7e275531d8ca5021ce5708980356eb422ab24f2
Maximilianus27 <haikalmaxim72@gmail.com>
2024-03-31 12:11:49 +0700

    update

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
-let txt = document.querySelector("#displayText");
+let txt      = document.querySelector("#displayText");
 let newTasks = document.querySelector("#addTasks");
-let boxList = document.querySelector("#list-tasks");
-
-const tasks = [
-    "Ngoding",
-    "Work Ot",
-    "Belajar Financial",
-    "Hapus ini",
-    "Gambar Animek"
-];
+let boxList  = document.querySelector("#list-tasks");
+let popUpBox = document.querySelector("#actions-pop-up");
+let tasks    = [];
 
 
 document.addEventListener("dblclick", (event)=>{randomTask();})
 document.addEventListener("keydown", (event)=>{ if(event.key == " "){randomTask();}})
 newTasks.addEventListener("keydown", (event)=>{ if(event.key == "Enter"){addTasks();}})
+newTasks.addEventListener("keydown", (event)=>{ 
+    if(newTasks.value !== "")
+    {document.querySelector("#btn-addTasks").classList.add("active");}
+    else
+    {document.querySelector("#btn-addTasks").classList.remove("active");}
+})
+
+
+function save(){
+    localStorage.setItem("savedList", JSON.stringify(tasks));
+    // popUp("Save berhasil dilakukan.", "info")
+}
+
+function load(){
+    tasks.splice(1, tasks.length);
+    let load = JSON.parse(localStorage.getItem("savedList"));
+    load.forEach((e)=>{
+        tasks.push(e);
+    })
+    // popUp("Load berhasil dilakukan.", "info")
+    updateList();
+}
 
 function randomTask() {
     let randNum = Math.floor(Math.random() * tasks.length);
@@ -38,18 +54,31 @@ function updateList() {
 function addTasks() {
     if (newTasks.value !== "") {
         tasks.push(newTasks.value);
-        console.log(newTasks.value + " Berhasil di tambahkan")
+        popUp(`${newTasks.value} berhasil di tambahkan!`, "succed")
         updateList();
+        save()
     }else{
-        console.log("Isi dulu!")
+        popUp("Isi dulu!","info")
     }
 
 }
 
 function delTasks(e) {
     console.log(e.parentNode.remove(e));
-    console.log(tasks.splice(e.id, 1) + " Berhasil di hapus");
+    popUp(`${tasks.splice(e.id, 1)} berhasil di hapus`, "danger");
     updateList();
+    save()
+}
+
+function popUp(txt, type){
+    let p = document.createElement("p");
+    p.id = type;
+    p.innerHTML = `<button onclick='delPopUp(this)''>x</button>${txt}`;
+    popUpBox.appendChild(p);
+}
+function delPopUp(e){
+    e.parentNode.remove(e);
 }
 
 updateList();
+load();
